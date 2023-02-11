7ebab736d6aae34dec033c4a2b3996f95b45b2ad
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-02-11 16:07:24 +0700

    add-resetall-feature

--- scoreboard/script.js
+++ scoreboard/script.js
@@ -1,35 +1,51 @@
 let pointHome = 0;
-let poinElHome = document.getElementById("point-el-home");
+let pointElHome = document.getElementById("point-el-home");
+function minus1home() {
+	pointHome--;
+	pointElHome.innerText = pointHome;
+}
 
 function plus1home() {
-  pointHome++;
-  poinElHome.innerText = pointHome;
+	pointHome++;
+	pointElHome.innerText = pointHome;
 }
 
 function plus2home() {
-  pointHome += 2;
-  poinElHome.innerText = pointHome;
+	pointHome += 2;
+	pointElHome.innerText = pointHome;
 }
 
 function plus3home() {
-  pointHome += 3;
-  poinElHome.innerText = pointHome;
+	pointHome += 3;
+	pointElHome.innerText = pointHome;
 }
 
-let pointElGuest = 0;
-let poinElGuest = document.getElementById("point-el-guest");
+let pointGuest = 0;
+let pointElGuest = document.getElementById("point-el-guest");
+
+function minus1guest() {
+	pointGuest--;
+	pointElGuest.innerText = pointGuest;
+}
 
 function plus1guest() {
-  pointElGuest++;
-  poinElGuest.innerText = pointElGuest;
+	pointGuest++;
+	pointElGuest.innerText = pointGuest;
 }
 
 function plus2guest() {
-  pointElGuest += 2;
-  poinElGuest.innerText = pointElGuest;
+	pointGuest += 2;
+	pointElGuest.innerText = pointGuest;
 }
 
 function plus3guest() {
-  pointElGuest += 3;
-  poinElGuest.innerText = pointElGuest;
+	pointGuest += 3;
+	pointElGuest.innerText = pointGuest;
+}
+
+function reset() {
+	pointHome = 0;
+	pointGuest = 0;
+	pointElGuest.innerText = pointGuest;
+	pointElHome.innerText = pointHome;
 }
