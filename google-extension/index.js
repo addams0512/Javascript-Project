c6372731c9bfe2328014a58b9d65cfc7b7db393d
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-02-19 13:07:44 +0700

    completed

--- google-extension/index.js
+++ google-extension/index.js
@@ -2,23 +2,48 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(
+	localStorage.getItem("myLeads", myLeads)
+)
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {
-	myLeads.push(inputEl.value)
-	inputEl.value = ""
-	renderLeads()
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage
+	render(myLeads)
+}
+
+tabBtn.addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myleads", JSON.stringify(myLeads))
+		render(myLeads)
+	})
 })
 
-const renderLeads = () => {
+let render = (dataOfarray) => {
 	let listItems = ""
-	for (i = 0; i < myLeads.length; i++) {
+	for (i = 0; i < dataOfarray.length; i++) {
 		listItems += `
 		<li>
-			<a target='_blank' href='${myLeads[i]}'>
-				${myLeads[i]}
+			<a target='_blank' href='${dataOfarray[i]}'>
+				${dataOfarray[i]}
 			</a>
 		</li>
 		`
 	}
 	ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+	localStorage.clear()
+	myLeads = []
+	render(myLeads)
+})
+
+inputBtn.addEventListener("click", () => {
+	myLeads.push(inputEl.value)
+	inputEl.value = ""
+	localStorage.setItem("myLeads", JSON.stringify(myLeads))
+	render(myLeads)
+})
